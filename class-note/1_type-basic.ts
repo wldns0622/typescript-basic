4ccb5bb83ac40d01dc16b4340b9ed04caecf6aae
wldns0622 <wldns12378@gmail.com>
2021-08-19 00:04:26 +0900

    Learn type2 in typescript

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,1 +1,29 @@
-var str = 'hello';
+// JS 문자열
+// var str = 'hello';
+
+// TS 문자열
+let str: string = 'hello';
+// TS 숫자
+let num: number = 10;
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let items: number[] = [1, 2, 3];
+
+// TS 튜플 : 배열의 각각 인덱스에 타입이 다 정해져있음
+let address: [string, number] = ['gangnam', 30];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+//   name: 'capt',
+//   age: 100,
+// };
+
+let person: { name: string; age: number } = {
+  name: '7juj7',
+  age: 29,
+};
+
+// TS 진위값
+let show: boolean = true;
