b256b3fcaa2fd376c044438e530bfc59de21e7fd
wldns0622 <wldns12378@gmail.com>
2021-08-19 00:16:13 +0900

    Learn functionType2 in typescript

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -1,7 +1,7 @@
 // 함수의 파라미터에 타입을 정의하는 방식
-function sum(a: number, b: number) {
-  return a + b;
-}
+// function sum(a: number, b: number) {
+//   return a + b;
+// }
 
 sum(10, 20);
 
@@ -10,7 +10,14 @@ function add(a, b): number {
   return a + b;
 }
 
-//함수에 타입을 정의하는 방식
+// 함수에 타입을 정의하는 방식
 function sum(a: number, b: number): number {
   return a + b;
 }
+sum(10, 20);
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string) {}
+
+log('hello world');
+log('hello ts', 'abc');
